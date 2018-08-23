7577d5bfae7a70440f99bbf1c8bca3136240c7a9
Philip Adams <pbadams@uchicago.edu>
2018-08-23 00:35:11 -0500

    Rendering full articles with comments

--- src/main.rs
+++ src/main.rs
@@ -77,7 +77,7 @@ fn main() {
     println!("{:?}", get_item(192327).unwrap());
     println!("{:?}", get_item(126809).unwrap());
     println!("{:?}", get_item(160705).unwrap());
-    println!("{}", render_compact(get_item(8863).unwrap()));
+    println!("{}", render_compact(&get_item(8863).unwrap()));
 
     let best = reqwest::get("https://hacker-news.firebaseio.com/v0/beststories.json");
 
@@ -87,7 +87,7 @@ fn main() {
             let bestids : Vec<u32> = serde_json::from_str(text).unwrap();
             println!("{:?}", bestids);
             for (i, id) in bestids.iter().enumerate() {
-                println!("{}", render_compact(get_item(*id).unwrap()));
+                println!("{}", render_compact(&get_item(*id).unwrap()));
                 if i > 25 {
                     break
                 }
@@ -96,7 +96,7 @@ fn main() {
         _ => ()
     }
 
-    
+    println!("{}", render_full(&get_item(8863).unwrap()));
     
 }
 
@@ -126,6 +126,50 @@ fn render_text(text : &str) -> String {
     from_read(text.as_bytes(), TEXT_COLS)
 }
 
-fn render_compact(item : Item) -> String {
-    format!("{}\n by {} {}", item.title.unwrap(), item.by, format_date(item.time))
+fn render_compact(item : &Item) -> String {
+    let score = match item.score {
+        Some(i) => format!("[{}] ", i),
+        _ => String::from(""),
+    };
+    let descendants = match item.descendants {
+        Some(i) => format!("({} comments)", i),
+        _ => String::from(""),
+    };
+    let title = match item.title {
+        Some(ref t) => t,
+        _ => "",
+    };
+    format!("{}{}\n by {} {} {}", score, title, item.by, format_date(item.time), descendants)
+}
+
+fn render_full(item : &Item) -> String {
+    let mut out = String::new();
+    match item.item_type {
+        ItemType::story | ItemType::job => out.push_str(&format!("{}\n", render_compact(item))),
+        ItemType::poll => {
+            match item.parts {
+                Some(ref parts) => {
+                    for opt in parts {
+                        out.push_str(&format!("{}\n", render_full(&get_item(*opt).unwrap())));
+                    }
+                },
+                _ => (),
+            }
+            
+        },
+        ItemType::pollopt => {
+            out.push_str(&format!("[{} votes] ", item.score.unwrap()));
+        },
+        ItemType::comment => {
+            out.push_str(&format!("{} {}\n", item.by, format_date(item.time)));
+        },
+    }
+    match item.text {
+        Some(ref text) => out.push_str(&format!("{}", render_text(text))),
+        _ => (),
+    }
+    for &kid in &item.kids {
+        out.push_str(&format!("{}\n", render_full(&get_item(kid).unwrap())));
+    }
+    out
 }
